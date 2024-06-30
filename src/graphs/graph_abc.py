dfc594b4d0eb69e78e3196b9deccde4234631b61
João Pedro Medrado Sena <joaopedromsena@gmail.com>
2024-06-30 16:00:23 -0300

    remove node args

--- src/graphs/graph_abc.py
+++ src/graphs/graph_abc.py
@@ -3,24 +3,13 @@ from abc import abstractmethod
 from typing import List
 from typing import Union
 from submissions.submission import Submission
-import json
+from pygraphviz import AGraph
 from networkx import Graph, MultiGraph
 
 class GraphABC(ABC):
-    _QUESTION_NODE_ARGS = {
-        'shape':'box',
-        'fontcolor':'white',
-        'style':'filled',
-        'fillcolor':'black'
-    }
-
-    _STUDENT_NODE_ARGS = {
-        'shape': 'circle',
-    }
-
     def __init__(self, submissions:List[Submission]=None) -> None:
         self._submissions = submissions
 
     @abstractmethod
-    def to_graph(self) -> Union[Graph, MultiGraph]:
+    def to_graph(self) -> Union[AGraph, Graph, MultiGraph]:
         pass
--- src/graphs/multigraph.py
+++ src/graphs/multigraph.py
@@ -1,22 +1,35 @@
 from graphs.graph_abc import GraphABC
-from networkx import MultiGraph as Multigraph
+from pygraphviz import AGraph
 
 class MultiGraph(GraphABC):
-    def to_graph(self, student_prefix:str='S', question_prefix:str='') -> Multigraph:
-        G = Multigraph()
+    _QUESTION_NODE_ARGS = {
+        'shape':'box',
+        'fontcolor':'white',
+        'style':'filled',
+        'fillcolor':'black'
+    }
 
-        for s in self._submissions:
-            student_node = student_prefix+str(s.student)
-            question_node = question_prefix+str(s.question)
+    _STUDENT_NODE_ARGS = {
+        'shape': 'circle',
+    }
+    
+    def to_graph(self, student_prefix:str='S', question_prefix:str='') -> AGraph:
+        G = AGraph(strict=False, directed=True)
 
-            G.add_node(student_node, **self._STUDENT_NODE_ARGS)
-            G.add_node(question_node, **self._QUESTION_NODE_ARGS)
+        for submission in self._submissions:
+            student_node = student_prefix+str(submission.student)
+            question_node = question_prefix+str(submission.question)
+
+            if not G.has_node(student_node):
+                G.add_node(student_node, **self._STUDENT_NODE_ARGS)
+
+            if not G.has_node(question_node):
+                G.add_node(question_node, **self._QUESTION_NODE_ARGS)
 
             G.add_edge(student_node,
                         question_node,
-                        color='green' if s.result else 'red',
-                        sign='+' if s.result else '-',
-                        time=s.time,
-                        label=f'{s.time}')
+                        color='green' if submission.result else 'red',
+                        sign='+' if submission.result else '-',
+                        label=f'{submission.spent}')
 
         return G
